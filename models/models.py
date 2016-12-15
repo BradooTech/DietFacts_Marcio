f96594f17f75cc5e39dd5af2cc11ed46d072d3e2
Marcio Ribeiro <marcio.ribeiro@bradootech.com>
2016-12-15 15:56:13 -0200

    adicionado o Meal Items

--- models/models.py
+++ models/models.py
@@ -9,6 +9,15 @@ class Dietfacts_product_template(models.Model):
 	calories = fields.Integer('Calories')
 	serving_size = fields.Float('Serving Size')
 	last_updated = fields.Date('Last Updated')
+	
+class DietFacts_res_users_meal(models.Model):
+	_name = 'res.users.meal'
+	name = fields.Char("Meal Name")
+	meal_date = fields.Datetime("Menu Date")
+	#item_ids = fields.One2many()
+	user_id = fields.Many2one('res.users','Meal User') 
+	notes = fields.Text('Meal Notes')
+	
 #	dietitem = fields.Boolean("Diet Item")
 #     _name = 'dietfacts.dietfacts'
 
