fa601f9c8332196ec77a5193fa01536068d09954
Marcio Ribeiro <marcio.ribeiro@bradootech.com>
2017-01-23 11:57:57 -0200

    modificações no custom_report

--- __manifest__.py
+++ __manifest__.py
@@ -25,8 +25,6 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
 	'views/dietfacts_view.xml',
     'views/assets.xml',
     'security/ir.model.access.csv',
